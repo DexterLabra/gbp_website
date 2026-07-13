748ff31db4fd28ed325bc6f98cd6fbb9602f2fa4
DexterLabra <dexterlabra789@gmail.com>
2026-07-13 10:10:17 +0800

    Refactor code structure for improved readability and maintainability

--- src/data/feedback.ts
+++ src/data/feedback.ts
@@ -4,7 +4,12 @@ export interface FeedbackItem {
 }
 
 export const feedbackItems: FeedbackItem[] = [
-    { src: "/assets/feedback1.jpg", alt: "Customer Feedback 1" },
-    { src: "/assets/feedback2.jpg", alt: "Customer Feedback 2" },
-    { src: "/assets/feedback3.jpg", alt: "Customer Feedback 3" },
+    { src: "/assets/FEEDBACK 1.png", alt: "Customer Feedback 1" },
+    { src: "/assets/FEEDBACK 2.png", alt: "Customer Feedback 2" },
+    { src: "/assets/FEEDBACK 3.png", alt: "Customer Feedback 3" },
+    { src: "/assets/FEEDBACK 4.png", alt: "Customer Feedback 4" },
+    { src: "/assets/FEEDBACK 5.png", alt: "Customer Feedback 5" },
+    { src: "/assets/FEEDBACK 6.png", alt: "Customer Feedback 6" },
+    { src: "/assets/FEEDBACK 7.png", alt: "Customer Feedback 7" },
+    
 ];
